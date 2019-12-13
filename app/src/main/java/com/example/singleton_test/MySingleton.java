f640037b629fb978a8124614637765f2b7ed1ef7
h86656519 <40815988+h86656519@users.noreply.github.com>
2019-12-13 09:48:51 +0800

    Update MySingleton.java

--- app/src/main/java/com/example/singleton_test/MySingleton.java
+++ app/src/main/java/com/example/singleton_test/MySingleton.java
@@ -4,8 +4,9 @@ public class MySingleton {
     private String data;
     static MySingleton mySingleton = new MySingleton(); //利用static 只能在記憶體中只有一份的特性，建立出singleton ，所以 static 就是必key
 
+    //建構子要設private，保護MySingleton 不會被其他地方用到
     private MySingleton() {
-    } //設private 保護MySingleton 不會被其他地方用到
+    } 
 
     public String getData() {
         return data;
